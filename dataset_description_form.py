ef032cdd9f6ece02c2a36d9f9ca91189f57727b7
Tolik <ump47g@gmail.com>
2020-10-07 17:43:11 +0300

    implemented data retrieval, needs testing.

--- dataset_description_form.py
+++ dataset_description_form.py
@@ -9,6 +9,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # static widgets with stored user input data
         self.derivative = None
         self.name_value = None
         self.bids_ver_value = None
@@ -87,7 +88,8 @@ class MainWindow(QMainWindow):
         data_type_label.setToolTip(tt.dataset_type)
         self.data_type_value = QComboBox()
         self.data_type_value.addItems(['unspecified', 'raw', 'derivative'])
-        self.data_type_value.currentIndexChanged.connect(self.dataset_type_handler)
+        self.data_type_value.currentIndexChanged.connect(
+            self.dataset_type_handler)
         self.layout_main.addWidget(data_type_label, row, 0)
         self.layout_main.addWidget(self.data_type_value, row, 1, 1, 3)
         row += 1
@@ -194,6 +196,7 @@ class MainWindow(QMainWindow):
 
         # save button, path selection section
         save_button = QPushButton("Save")
+        save_button.clicked.connect(self.test_get_data)
         save_as_button = QPushButton("Save as")
         self.layout_main.addWidget(save_button, row, 1)
         self.layout_main.addWidget(save_as_button, row, 2)
@@ -282,7 +285,9 @@ class MainWindow(QMainWindow):
 
         if self.derivative is not None:
             data_derivative = self.derivative.get_data()
-            #TODO: add to data from data_derivative
+            data['GeneratedBy'] = data_derivative['GeneratedBy']
+            data['SourceDatasets'] = data_derivative['SourceDatasets']
+        return data
 
-
-
+    def test_get_data(self):
+        print(self.get_data())
