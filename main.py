eb5908c9f3016b1e24e379eaeb2082073345ca78
Dean Geckt <degeckt@microsoft.com>
2020-10-07 16:06:31 +0300

    root gitignore + spaces

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from dataset_description_form import MainWindow
 from PyQt5.QtWidgets import QApplication, QScrollArea
 import sys
 
-
-
-
 app = QApplication([])
 window = MainWindow()
 sys.exit(app.exec())
