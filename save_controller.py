c3887ee4de5355702f163975fd5c5c7d6c50ba5d
Point Blank <ump47g@gmail.com>
2020-10-18 20:00:38 +0300

    save controller changed from 2 buttons to a single button, Qdialog now selects a directory and not a file

--- save_controller.py
+++ save_controller.py
@@ -1,15 +1,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 import json
-import ntpath
-
 from utils import remove_empty_fields
+import os
 
 
 class SaveController(QWidget):
     def __init__(self, get_data_callback):
         super().__init__()
-        self.save_as_button = QPushButton('Save As')
         self.save_button = QPushButton('Save')
         self.is_valid_text = QLabel('Missing required fields')
         self.is_valid_icon = QLabel()
@@ -18,20 +16,18 @@ class SaveController(QWidget):
         self.curr_file_name = None
         self.get_data = get_data_callback
         self.init_ui()
+        self.last_dir = None
 
     def init_ui(self):
         row = 0
         self.is_valid_icon.setPixmap(QPixmap('icons/invalid.png'))
         self.is_valid_icon.setFixedWidth(20)
         self.layout.addWidget(self.save_button, row, 0)
-        self.layout.addWidget(self.save_as_button, row, 1)
-        self.layout.addWidget(self.is_valid_icon, row, 2)
-        self.layout.addWidget(self.is_valid_text, row, 3)
-        self.layout.addItem(QSpacerItem(0, 0), row, 4)
+        self.layout.addWidget(self.is_valid_icon, row, 1)
+        self.layout.addWidget(self.is_valid_text, row, 2)
+        self.layout.addItem(QSpacerItem(0, 0), row, 3)
 
-        self.save_as_button.clicked.connect(self.save_as)
-        self.save_as_button.setDisabled(True)
-        self.save_button.clicked.connect(self.save_data_as_json)
+        self.save_button.clicked.connect(self.save)
         self.save_button.setDisabled(True)
 
     def set_valid(self, valid):
@@ -41,28 +37,22 @@ class SaveController(QWidget):
         else:
             self.is_valid_icon.setPixmap(QPixmap('icons/invalid.png'))
             self.is_valid_text.setText("Missing required fields")
-
-        self.save_button.setDisabled(not valid or not self.curr_file_name)
-        self.save_as_button.setDisabled(not valid)
+        self.save_button.setDisabled(not valid)
 
     def save_data_as_json(self):
         cleaned_data = remove_empty_fields(self.get_data())
-        with open(self.curr_file_name, 'w', encoding='utf-8') as f:
+        f_path = os.path.join(self.last_dir, 'dataset_description.json')
+        with open(f_path, 'w', encoding='utf-8') as f:
             json.dump(cleaned_data, f, indent=4)
 
-    def save_as(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getSaveFileName(self, "Save as", "",
-                                                   "All Files (*);;Text Files (*.txt)",
-                                                   options=options)
-        if not file_name or file_name == '':
-            print('failed to save new file')
+    def save(self):
+        if self.last_dir:
+            kwargs = {'caption': 'Select Directory',
+                      'directory': self.last_dir}
+        else:
+            kwargs = {'caption': 'Select Directory'}
+        file = str(QFileDialog.getExistingDirectory(**kwargs))
+        if file == '':
             return
-
-        if not file_name.endswith('.json'):
-            file_name += '.json'
-
-        self.save_button.setDisabled(False)
-        self.curr_file_name = file_name
+        self.last_dir = file
         self.save_data_as_json()
